6adbb6de839e91b093fd3e42364b550ed17f4130
pedro2398 <pedrooo2398@gmail.com>
2024-03-21 13:01:07 -0300

    first commit

---
+++ src/test/java/com/AppMusic/AppMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
