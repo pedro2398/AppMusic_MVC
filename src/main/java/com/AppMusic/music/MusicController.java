675e9b94feb1c22998b8b1cf4c094f46fef7e966
pedro2398 <pedrooo2398@gmail.com>
2024-03-23 06:30:40 -0300

    adding CSS and improving tha application

--- src/main/java/com/AppMusic/music/MusicController.java
+++ src/main/java/com/AppMusic/music/MusicController.java
@@ -1,25 +1,53 @@
 package com.AppMusic.music;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("music")
 public class MusicController {
+    @Autowired
+    MusicRepository repository;
 
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("music", List.of(
-                "Musica 1",
-                "Musica 2",
-                "MUsica 3",
-                "MUsica 4"
-        ));
+        model.addAttribute("music", repository.findAll());
 
         return "music/index";
+    }
+
+    @GetMapping("delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirect){
+        Optional<Music> music = repository.findById(id);
+        if(music.isEmpty()) {
+            redirect.addFlashAttribute("message", "Music can't be deleted!");
+
+            return "redirect:/music";
+        }
+
+         redirect.addFlashAttribute("message", "Music Deleted!");
+         repository.deleteById(id);
+
+         return "redirect:/music";
+    }
+
+    @GetMapping("new")
+    public String form() {
+        return "music/form";
+    }
+
+    @PostMapping
+    public String add(Music music, RedirectAttributes redirect) {
+        repository.save(music);
+        redirect.addFlashAttribute("message", "Music Added!");
+        return "redirect:/music";
     }
 }
---
+++ src/main/java/com/AppMusic/music/MusicRepository.java
@@ -0,0 +1,7 @@
+package com.AppMusic.music;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface MusicRepository extends JpaRepository<Music, Long> {
+}
